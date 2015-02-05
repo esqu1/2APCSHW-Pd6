8129379521f4ca7e37ef3e4b2079e592a0e9ee47
esqu1 <brandbest1@gmail.com>
2015-02-04 23:39:54 -0500

    recursion + gitignore

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,18 @@
+public class Recursion{
+
+    public String name(){
+	return "Lin,Brandon";
+    }
+
+    public int fact(int n){
+
+    }
+
+    public int fib(int n){
+
+    }
+
+    public double sqrt(double n){
+
+    }
+}
