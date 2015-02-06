18ff139ce15713f6bc6b927ed6bc6c0b34e4106b
esqu1 <brandbest1@gmail.com>
2015-02-05 19:44:56 -0500

    recursion methods added

---
+++ 01recursion/Driver.java
@@ -0,0 +1,13 @@
+public class Driver{
+
+    public static void main(String[] args){
+	Recursion r = new Recursion();
+	System.out.println(r.name());
+	System.out.println(r.fact(5));
+	//System.out.println(r.fact(-1));
+	System.out.println(r.fib(4));
+	//System.out.println(r.fib(-5));
+	System.out.println(r.sqrt(2));
+    }
+}
+    
--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,18 +1,46 @@
-public class Recursion{
+public class Recursion implements hw1{
 
     public String name(){
 	return "Lin,Brandon";
     }
 
     public int fact(int n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if(n == 0){
+	    return 1;
+	}
+	return n * fact(n-1);
+    }
 
+    public int fibHelp(int fib1, int fib2, int count){
+	if(count == 1){
+	    return fib2;
+	}
+	if(count == 0){
+	    return 0;
+	}
+	return fibHelp(fib2, fib1+fib2, count - 1);
     }
 
     public int fib(int n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
+	return fibHelp(0,1,n);
+    }
 
+    public double sqrtHelp(double n, double guess){
+	double EPSILON = 0.000001;
+	double newguess = (n / guess + guess) / 2;
+	if(guess - newguess <= EPSILON){
+	    return newguess;
+	}
+	return sqrtHelp(n, newguess);
     }
 
     public double sqrt(double n){
-
+	return sqrtHelp(n, n / 2);
     }
 }
