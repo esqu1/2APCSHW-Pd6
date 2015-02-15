a4eec95f376936cd133ccd41c80181704ec02dcf
esqu1 <brandbest1@gmail.com>
2015-02-15 00:33:14 -0500

    consolidated driver to main method in class

--- 02knightstour/KnightsTour.java
+++ 02knightstour/KnightsTour.java
@@ -85,6 +85,22 @@ public class KnightsTour{
 	}
 	return false;
     }
+
+    public static void main(String[] args){
+	int size = 0;
+	try{
+	    size = Integer.parseInt(args[0]);
+	}catch(Exception e){
+	    size = 5;
+	}
+	KnightsTour k = new KnightsTour(size);
+	//System.out.println(k);
+	if(k.solve(0,0,1)){
+	    System.out.println(k);
+	}else{
+	    System.out.println("no solution");
+	}
+    }
     
     
 }
--- 03nqueens/NQueens.java
+++ 03nqueens/NQueens.java
@@ -69,5 +69,18 @@ public class NQueens{
 	board[row][col] = '.';
 	return false;
     }
+
+    public static void main(String[] args){
+	int size;
+	try{
+	    size = Integer.parseInt(args[0]);
+	}catch(Exception e){
+	    size = 8;
+	}
+	NQueens n = new NQueens(size);
+	if(n.solve()){
+	    System.out.println(n);
+	}
+    }
 		
 }
