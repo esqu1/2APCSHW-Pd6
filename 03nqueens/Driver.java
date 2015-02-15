24ddb39b1030f9d4551c4d8576961f52bcc0b7fc
esqu1 <brandbest1@gmail.com>
2015-02-15 00:00:46 -0500

    phew, nqueens finally works

--- 03nqueens/Driver.java
+++ 03nqueens/Driver.java
@@ -1,7 +1,9 @@
 public class Driver{
 
     public static void main(String[] args){
-	NQueens n = new NQueens(8);
-	System.out.println(n);
+	NQueens n = new NQueens(9);
+	if(n.solve(0)){
+	    System.out.println(n);
+	}
     }
 }
--- 03nqueens/NQueens.java
+++ 03nqueens/NQueens.java
@@ -19,5 +19,75 @@ public class NQueens{
 	}
 	return out;
     }
+
+    public void wait(int millis){
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
+    }
+
+    public boolean solve(){
+	for(int i = 0; i < board.length; i++){
+	    if(solve(i)){
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public boolean solve(int x){
+	board[0][x] = 1;
+	boolean val = false;
+	for(int i = 0; i < board[0].length; i++){
+	    val = val || solveHelp(x,1,i);
+	}
+	if(val){
+	    return true;
+	}
+	board[0][x] = 0;
+	return false;
+    }
+
+    public boolean solveHelp(int x, int row, int col){
+	//System.out.println(row + "." + col);
+	//System.out.println(this);
+	//wait(20);
+	boolean valid = true;
+	if(row == board.length){
+	    return true;
+	}
+	if(col == board[0].length){
+	    return false;
+	}
+	for(int i = 0; i < board.length; i++){
+	    for(int j = 0; j < board[0].length; j++){
+		//System.out.println(board[i][j]);
+		//System.out.println(i + " " + j);
+		//System.out.println(row + "?" + col);
+		if( !(row == i && row == j) && board[i][j] == 1 && ((Math.abs(row - i) == Math.abs(col - j)) || row == i || col == j) ){
+		    //System.out.println("meh");
+		    valid = false;
+		    break;
+		}
+	    }
+	}
+
+	if(!valid){
+	    return false;
+	}
+
+	board[row][col] = 1;
+	valid = false;
+	for(int i = 0; i < board[0].length; i++){
+	    valid = valid || solveHelp(x,row+1,i);
+	}
+	if(valid){
+	    return true;
+	}
+	board[row][col] = 0;
+	return false;
+    }
 		
 }
