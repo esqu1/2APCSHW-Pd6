1876bd6df4eae56147428f0584eb8c4e1cb83e19
esqu1 <brandbest1@gmail.com>
2015-03-11 17:11:38 -0400

    add index and indexOf

--- 07LinkedList/MyLinkedList.java
+++ 07LinkedList/MyLinkedList.java
@@ -8,6 +8,7 @@ public class MyLinkedList{
 	while(true){
 	    if(now != null){
 		result += now.getData() + " ";
+		now = now.getNext();
 	    }else{
 		break;
 	    }
@@ -50,6 +51,48 @@ public class MyLinkedList{
 	}
 	return now.getData();
     }
+
+    public void set(int index, int value){
+	int n = 0;
+	LNode now = first;
+	for (int i = 0; i < index; i++) {
+	    now = now.getNext();
+	}
+	now.setData(value);
+    }
+
+    public int indexOf(int value){
+	int n = 0;
+	LNode now = first;
+	while(true){
+	    if(now.getData() == value){
+		return n;
+	    }
+	    now = now.getNext();
+	    n++;
+	}
+    }
+
+    public void add(int index, int value){
+	int n = 0;
+	LNode now = first;
+	for (int i = 0; i < index - 1; i++) {
+	    now = now.getNext();
+	}
+	LNode tobe = new LNode(value,now.getNext());
+	now.setNext(tobe);
+    }
+
+    public static void main(String[] args){
+	MyLinkedList l = new MyLinkedList();
+	l.add(3);
+	l.add(4);
+	System.out.println(l.get(0));
+	System.out.println(l.get(1));
+	System.out.println(l);
+
+
+
+    }
     
-   
 }
