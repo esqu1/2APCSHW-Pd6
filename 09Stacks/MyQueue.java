601fe30f925f22334ce02fe895f391511ec8e25b
esqu1 <brandbest1@gmail.com>
2015-03-23 12:30:19 -0400

    queue done

--- 09Stacks/MyQueue.java
+++ 09Stacks/MyQueue.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class MyQueue<T>{
-    private LinkedList<T> list;
+    private MyLinkedList<T> list;
 
-    public MyQueue(LinkedList<T> l){
+    public MyQueue(MyLinkedList<T> l){
 	list = l;
     }
 
@@ -10,6 +10,23 @@ public class MyQueue<T>{
 	if(t == null){
 	    throw new NullPointerException();
 	}
+	list.add(t);
+	return true;
+    }
+
+    public T remove(){
+	T lol = list.get(0);
+	list.remove(0);
+	return lol;
+	
+    }
+
+    public static void main(String[] args) {
+	MyQueue<Integer> q = new MyQueue<Integer>(new MyLinkedList<Integer>());
+	q.add(2);
+	q.add(4);
+	System.out.println(q.remove());
+	System.out.println(q.remove());
 
     }
     
