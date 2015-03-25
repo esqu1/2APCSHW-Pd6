3ee55424ec0a7b2165e4968b6ecf7ce5eb188fe8
esqu1 <brandbest1@gmail.com>
2015-03-25 12:34:02 -0400

    :sushi: templates

--- 10DoubleEdgedSword/MyDeque.java
+++ 10DoubleEdgedSword/MyDeque.java
@@ -2,4 +2,34 @@ import java.util.*;
 public class MyDeque<T>{
   private T[] list;
 
+  public MyDeque(){
+  
+  }
+  public void addFirst(T value){
+
+  }
+
+  public void addLast(T value){
+
+  }
+
+  public T removeFirst(){
+
+  }
+
+  public T removeLast(){
+
+  }
+
+  public T getFirst(){
+
+  }
+
+  public T getLast(){
+
+  }
+
+  public static void main(String[] args){
+
+  }
 }
