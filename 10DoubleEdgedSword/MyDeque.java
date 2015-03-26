fe274150dad6d63a40e9fe8af7c74adf8647b012
esqu1 <brandbest1@gmail.com>
2015-03-25 23:30:07 -0400

    :smile: constructor + addfirst

--- 10DoubleEdgedSword/MyDeque.java
+++ 10DoubleEdgedSword/MyDeque.java
@@ -1,17 +1,35 @@
 import java.util.*;
 public class MyDeque<T>{
   private Object[] list;
-  private Object head, tail;
+  private int head, tail;
   private int size;
 
   public MyDeque(){
     list = new Object[10];
-    head = list[0];
-    tail = list[0];
+    head = 0;
+    tail = 0;
+  }
+
+  public void resize(){
+    Object[] newlist = new Object[size * 2];
+    for(int i = 0; i < size; i++){
+      newlist[i] = list[(head + i) % size];
+    }
+    list = newlist;
+    head = 0;
+    tail = size - 1;
   }
 
   public void addFirst(T value){
-
+    if(size == list.length){
+      resize();
+    }
+    head--;
+    if(head < 0){
+      head += size;
+    }
+    list[head] = value;
+    size += 1;
   }
 
   public void addLast(T value){
