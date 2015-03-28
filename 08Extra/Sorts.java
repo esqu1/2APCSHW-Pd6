003bb21ab25799953ad701c3faa72cc2ca198d3b
esqu1 <brandbest1@gmail.com>
2015-03-28 11:13:33 -0400

    my quicksort can't get any quicker :trollface:

--- 08Extra/Sorts.java
+++ 08Extra/Sorts.java
@@ -51,17 +51,14 @@ public class Sorts{
     // QUICK SORT
     /////////////////////////    
     public static void swap(int[] L, int i1, int i2){
-	if(i1 == i2){
-	    return;
-	}
+	//if(i1 == i2) return;
 	int temp = L[i1];
 	L[i1] = L[i2];
 	L[i2] = temp;
-	
     }
 
     public static void insertion(int[] c, int start, int end){
-	for(int i = start + 1; i < end; i++){
+	for(int i = start + 1; i <= end; i++){
 	    int j = i;
 	    int temp = c[j];
 	    while(j > 0 && temp < c[j-1]){
@@ -73,53 +70,30 @@ public class Sorts{
     }
 
     public static void partition(int[] L, int start, int end){
-	//System.out.println("Start: " + start + ", End: " + end);
-	if(end - start < 3){
-	    insertion(L,start,end);
-	    return;
-	}
 	if(start >= end){
 	    return;
 	}
-	Random r = new Random();
-	int pivot1 = L[start];
-	int pivot2 = L[end];
-	/*if(pivot1 == pivot2){
-	    while(pivot1 == pivot2){
-		pivot1 = L[(int)(Math.random() * (end - start)) + start];
-	    }
-	    }*/
-	if(pivot1 > pivot2){
-	    int temp = pivot1;
-	    pivot1 = pivot2;
-	    pivot2 = temp;
+	
+	if(end - start < 5){
+	    insertion(L,start,end);
+	    return;
 	}
-	//System.out.println("Pivot: " + pivot1);
-	//System.out.println("Pivot: " + pivot2);
-	int low = start+1, mid = low, high = end-1;
-	while(mid <= p2){
-	    if(L[mid] < pivot1){
-		swap(L,mid,low);
+	int pivot = L[(int)(Math.random() * (end - start)) + start];
+	int low = start, mid = start, high = end;
+	while(mid <= high){
+	    if(L[mid] < pivot){
+		swap(L,low,mid);
 		low++;
 		mid++;
-	    }else if(L[mid] > pivot2){
+	    }else if(L[mid] > pivot){
 		swap(L,mid,high);
-		if(p2 != high){
-		    swap(L,p2,mid);
-		}
-		p2--;
 		high--;
-	    }	    
-	    
+	    }else{
+		mid++;
+	    }
 	}
-	
-
-
-	//System.out.println(Arrays.toString(L));
-
 	partition(L,start,low);
-	partition(L,p1,p2);
-	partition(L,high,end);
+	partition(L,mid,end);
 	
     }
 
@@ -164,41 +138,30 @@ public class Sorts{
 	int[] c2 = Arrays.copyOf(c1,10000000);
 	int[] c3 = Arrays.copyOf(c1,10000000);
 	int[] c4 = Arrays.copyOf(c1,10000000);
-	int[] c5 = new int[100000];
-	for(int i = 0; i < c5.length; i++){
-	    c5[i] = 0;
-	}
+	int[] c5 = Arrays.copyOf(c1,10000000);
+	int[] c6 = Arrays.copyOf(c1,10000000);
 
 	int[] c = {4,2,7,3,7,0,1,34,6,8,2};
 	quick(c);
 	System.out.println(Arrays.toString(c));
-
 	/*
-
 	long startTime = System.currentTimeMillis();
 	merge(c1);
 	long endTime = System.currentTimeMillis();
 	System.out.println("MergeSort: " + (endTime - startTime));
 	*/
-	long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
 	quick(c2);
 	long endTime = System.currentTimeMillis();
-	System.out.println("QuickSort: " + (endTime - startTime));
-	/*
-	startTime = System.currentTimeMillis();
-	radix(c3);
-	endTime = System.currentTimeMillis();
-	System.out.println("RadixSort: " + (endTime - startTime));
+	System.out.println("QuickSort 20: " + (endTime - startTime));
+
 	
 	startTime = System.currentTimeMillis();
-	Arrays.sort(c4);
+	Arrays.sort(c6);
 	endTime = System.currentTimeMillis();
 	System.out.println("Arrays.sort(): " + (endTime - startTime));
-	*/
-	startTime = System.currentTimeMillis();
-	quick(c5);
-	endTime = System.currentTimeMillis();
-	System.out.println("DupQuickSort: " + (endTime - startTime));
+       
+
 
 	
 	
