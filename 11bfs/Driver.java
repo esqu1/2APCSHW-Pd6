027e8cec3b43da29cb68fedf2b956730ddc619d3
esqu1 <brandbest1@gmail.com>
2015-04-19 22:29:14 -0400

    added new sample maze

--- 11bfs/Driver.java
+++ 11bfs/Driver.java
@@ -4,7 +4,7 @@ public class Driver{
   public static void main(String[] args) {
     Maze m = new Maze(args[0]);
     m.solve(Integer.parseInt(args[1]),true);
-    //System.out.println(m);
+    System.out.println(m);
     System.out.println(Arrays.toString(m.solutionCoordinates()));
   }
 
