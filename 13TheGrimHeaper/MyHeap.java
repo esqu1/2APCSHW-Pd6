10f02f551d04e0a511e51d8e94d7e242df391046
esqu1 <brandbest1@gmail.com>
2015-05-12 07:08:01 -0400

    new comment inserted, finished addHelp

--- 13TheGrimHeaper/MyHeap.java
+++ 13TheGrimHeaper/MyHeap.java
@@ -14,6 +14,7 @@ public class MyHeap{
     public int compare(int a, int b){
 	// for MAXHEAP: returns 1 if a > b, 0 otherwise.
 	// for MINHEAP: returns 1 if a < b, 0 otherwise.
+	// convention: compare(parent, child)
 	return mode == MAXHEAP ? (a > b ? 1 : 0) : (a < b ? 1 : 0);
     }
 	
@@ -31,7 +32,20 @@ public class MyHeap{
     public void add(int value){
 	resize();
 	data[data[0] + 1] = value;
-	
+	addHelp(data[0] + 1);
+    }
+
+    public void addHelp(int index){
+	if(compare(data[index / 2],data[index]) == 0){
+	    int temp = data[index / 2];
+	    data[index / 2] = data[index];
+	    data[index] = temp;
+	    addHelp(index / 2);
+	}
+    }
+
+    public int remove(){
+	int value = data[1];
     }
     
 }
