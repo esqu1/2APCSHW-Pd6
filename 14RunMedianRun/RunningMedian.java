2a01437c240f9f15e7140baa2e7b5d2c080f38ed
esqu1 <brandbest1@gmail.com>
2015-05-14 12:13:58 -0400

    it compiles!

--- 14RunMedianRun/RunningMedian.java
+++ 14RunMedianRun/RunningMedian.java
@@ -4,5 +4,37 @@ public class RunningMedian{
     private boolean oneValueOrNah;
     private int median;
 
-    
+    public RunningMedian(){
+	h1 = new MyHeap(true); //max heap, left
+	h2 = new MyHeap(false);//min heap, right
+	oneValueOrNah = false;
+    }
+
+    public double getMedian(){
+	return oneValueOrNah ? median : ((double) h1.peek() + h2.peek()) / 2;
+    }
+
+    public void add(int value){
+	if(oneValueOrNah){
+	    if(value > median){
+		h1.add(median);
+		h2.add(value);	
+	    }else{
+		h1.add(value);
+		h2.add(median);
+	    }
+	    oneValueOrNah = false;
+	}else{
+	    if(value > h2.peek()){
+		median = h2.remove();
+		h2.add(value);
+	    }else if(value < h1.peek()){
+		median = h1.remove();
+		h1.add(value);
+	    }else{
+		median = value;
+	    }
+	    oneValueOrNah = true;
+	}
+    }
 }
